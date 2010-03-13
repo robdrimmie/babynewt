2cd4111792f30a73899cd1df7eb5472d8874a63d
Finn Smith <finn@timeghost.net>
2010-03-13 15:30:29 -0500

    Fixed indentation.

--- Database.php
+++ Database.php
@@ -44,18 +44,18 @@ class Database {
      */
     public static function sql_quote( $value ) {
 
-      if ( get_magic_quotes_gpc() ) {
-        $value = stripslashes( $value );
-      }
-      //check if this function exists
-      if ( function_exists( "mysql_real_escape_string" ) ) {
-        $value = mysql_real_escape_string( $value );
-      }
-      //for PHP version < 4.3.0 use addslashes
-      else {
-        $value = addslashes( $value );
-      }
-      return $value;
+        if ( get_magic_quotes_gpc() ) {
+            $value = stripslashes( $value );
+        }
+        // check if this function exists
+        if ( function_exists( "mysql_real_escape_string" ) ) {
+            $value = mysql_real_escape_string( $value );
+        }
+        // for PHP version < 4.3.0 use addslashes
+        else {
+            $value = addslashes( $value );
+        }
+        return $value;
     }
 
 }
