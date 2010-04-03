949725bc44b5a24d44e950efec0ac622c6de7ccf
daveadams <daveadams@gmail.com>
2010-04-03 07:52:36 -0400

    favorites.php: updated to include session.php

--- favorites.php
+++ favorites.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-include("include.php");
+    include("session.php");
+    include("include.php");
 
 // establish connection to MySQL database or output error message.
 $link = mysql_connect ($dbHost, $dbUser, $dbPassword);
