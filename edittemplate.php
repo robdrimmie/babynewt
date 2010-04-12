5fe026f85ac28e269c93117cc66fff71ed6e82e0
Lint <evanescence3@yahoo.com>
2010-04-12 09:55:29 +0800

    EditTemplate.php to edittemplate.php

--- edittemplate.php
+++ edittemplate.php
@@ -106,7 +106,7 @@ $TemplateQuery = "SELECT i_TemplateID
                 ORDER BY i_TemplateID";
 $TemplateQueryId = mysql_query ($TemplateQuery, $link);
 
-echo "<form name=\"frmSelection\" action=\"EditTemplate.php\" method=\"post\">\n";
+echo "<form name=\"frmSelection\" action=\"edittemplate.php\" method=\"post\">\n";
 echo "Your Templates: <select name=\"selTemplate\">\n";
 if ( Empty($selTemplate) && !Empty($hdnTemplateId)) {
     $selTemplate = $hdnTemplateId;
@@ -133,7 +133,7 @@ $TemplateQuery = "SELECT i_TemplateID, vc_TemplateName
                                     ORDER BY i_TemplateID";
 $TemplateQueryId = mysql_query ($TemplateQuery, $link);
 
-echo "<form name=\"frmSelection\" action=\"EditTemplate.php\" method=\"post\">\n";
+echo "<form name=\"frmSelection\" action=\"edittemplate.php\" method=\"post\">\n";
 echo "Public Templates: <select name=\"selTemplate\">\n";
 while ( $Templates = mysql_fetch_object($TemplateQueryId) ) {
     echo "<option value=\"$Templates->i_TemplateID\"";
@@ -163,7 +163,7 @@ if (  Empty($btnMakeMine)
         $selTemplate = $hdnTemplateId;
     }
     // Create a text area
-    echo "<form name=\"frmTemplate\" action=\"EditTemplate.php\" method=\"post\">";
+    echo "<form name=\"frmTemplate\" action=\"edittemplate.php\" method=\"post\">";
     echo "<input type=\"hidden\" name=\"hdnTemplateId\" value=\"";
     if ( !Empty( $btnNewTemplate ) ) {
         // if the button for a new stylesheet has been selected
@@ -237,7 +237,7 @@ if (  Empty($btnMakeMine)
     echo "Your selected template has been updated.";
 }
 
-echo "<form name=\"frmNewStylesheet\" action=\"EditTemplate.php\" method=\"post\">";
+echo "<form name=\"frmNewStylesheet\" action=\"edittemplate.php\" method=\"post\">";
 echo "<input type=\"submit\" name=\"btnNewTemplate\" value=\"New Template\">";
 echo "</form>";
 
