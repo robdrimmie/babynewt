4cfc67e285575b0e1dbf57df18a4fa2f62b66947
Rob Drimmie <rob.drimmie@gmail.com>
2012-10-27 19:30:24 -0400

    Restructured Database.php to handle different connection styles.
    A few things happening. Comment insertion is broken right now because
    apostrophes aren't handled properly because the original is stupid.
    
    If I'm going to fix a query I'm going to move towards the class-based
    structure I have in mind, which means a bit of background work is
    necessary. I can't just force it into using a prepared statement with
    mysqli (which is what I tried doing in previous commits) because there
    are different connection mechanisms vs old style mysql_connect() sort of
    stuff.
    
    So: Database.php now handles different connection types. The vast
    majority of the site will never encounter it but post.php will be
    updated to use the new stuff.
    
    Anyway it's not super great, a lot of dupey dupey, but at least now
    instances of the site won't need to manage both Database.php and
    include.php.

---
+++ Database.php
@@ -0,0 +1,106 @@
+<?php
+
+class Database {
+    protected $_name;
+    protected $_user;
+    protected $_password;
+    protected $_host;
+    protected $_oldConnection;
+    protected $_connection;
+
+    public function __construct( $dbProperties = array(), $old = false, $new = true ) {
+        $defaults = array(
+            'name' => 'babynewt',
+            'user' = 'babynewt',
+            'password' = 'babynewt',
+            'host' = 'localhost',
+        );
+
+        $merged = array_merge( $defaults, $dbProperties );
+
+        $this->_name = merged['name'];
+        $this->_user = merged['user'];
+        $this->_password = merged['password'];
+        $this->_host = merged['host'];
+
+        $this->_oldConnection = null;
+        $this->_connection = null;
+
+        if( $old ) {
+            $this->getOldConnection();
+        }
+
+        if( $new ) {
+            $this->getConnection();
+        }
+    }
+
+
+    protected function _connectOld() {
+        if ( null === $this->_oldConnection ) {
+            $this->_oldConnection = mysql_connect( 
+                $this->_host, 
+                $this->_user, 
+                $this->_password 
+            );
+
+            if ( !mysql_select_db( $this->_name, $this->_oldConnection ) ) {
+                throw new Exception( 
+                    'Unable to select old database: [' . mysql_errno() . 
+                    ']' . mysql_error() 
+                );
+            }
+        }
+
+        return $this->_conn;
+    }
+
+    protected function _connect() {
+        if ( null === $this->_connection ) {
+            $this->_connection = new mysqli( 
+                $this->_host, 
+                $this->_user, 
+                $this->_password,
+                $this->_name,
+            );
+
+            if ( mysqli->connect_errno ) {
+                throw new Exception( 
+                    'Unable to select  database: [' . mysqli->connect_errno . 
+                    ']' . mysqli->connect_error
+                );
+            }
+        }
+
+        return $this->_conn;
+    }
+
+    public function getOldConnection() {
+        return $this->_connectOld();
+    }
+
+    public function getConnection() {
+        return $this->_connect();
+    }
+
+
+    /**
+     * @see http://www.askbee.net/articles/php/SQL_Injection/sql_injection.html
+     */
+    public static function sql_quote( $value ) {
+
+        if ( get_magic_quotes_gpc() ) {
+            $value = stripslashes( $value );
+        }
+        // check if this function exists
+        if ( function_exists( "mysql_real_escape_string" ) ) {
+            $value = mysql_real_escape_string( $value );
+        }
+        // for PHP version < 4.3.0 use addslashes
+        else {
+            $value = addslashes( $value );
+        }
+        return $value;
+    }
+
+}
